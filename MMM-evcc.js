6e745bade29db4eca5e9287fe3555d6a5c3c5149
Andre Oestreich <mail@andreoestreich.de>
2024-05-18 03:58:28 +0200

    added pv only mode, update interval, css, icons

--- MMM-evcc.js
+++ MMM-evcc.js
@@ -3,7 +3,23 @@ Module.register("MMM-evcc", {
     defaults: {
         ip: "localhost",
         port: "7070",
+        decimalDigits: 2,
+        updateInterval: 30,
+        displayMode: "all",
     },
+
+    // Define required scripts.
+    getScripts: function () {
+        return [
+            "https://cdn.jsdelivr.net/npm/@h2d2/shopicons@1.2.0/umd/regular.js",            
+            "MMM-evcc.css",
+        ];
+    },
+
+    // Define required styles.
+    /*getStyles: function () {
+        return ["MMM-evcc.css"];
+    },*/
 
     // Define start sequence.
     start: function () {
@@ -12,6 +28,7 @@ Module.register("MMM-evcc", {
         this.evccData = {};
         this.sendSocketNotification("SET_EVCC_CONFIG", this.config);
         this.sendSocketNotification("GET_EVCC_DATA");
+        this.scheduleUpdate();
     },
 
     // Override dom generator.
@@ -22,10 +39,13 @@ Module.register("MMM-evcc", {
             return wrapper;
         }
         // Display pvPower
-        if (this.evccData && this.evccData.pvPower !== undefined) {
-            var pvPowerElement = document.createElement("div");
-            pvPowerElement.innerHTML = "PV Power: " + this.evccData.pvPower + " W";
-            wrapper.appendChild(pvPowerElement);
+        if (this.config.displayMode == "pv") {
+            if (this.evccData && this.evccData.pvPower !== undefined) {
+                var pvPowerElement = document.createElement("div");
+                pvPowerElement.className = "flex large";
+                pvPowerElement.innerHTML = "<span style=\"margin-right: 15px\"><shopicon-regular-sun size=\"xl\"></shopicon-regular-sun></span> " + this.formatWatts(this.evccData.gridPower)  + "</div>";
+                wrapper.appendChild(pvPowerElement);
+            }
         }
         return wrapper;
     },
@@ -38,4 +58,20 @@ Module.register("MMM-evcc", {
             this.updateDom();
         }
     },
+    
+    // Format a number with the configured number of decimal digits.
+    formatWatts(value) {
+        return (value/1000).toLocaleString("de-DE", { minimumFractionDigits: this.config.decimalDigits, maximumFractionDigits: this.config.decimalDigits }) + " kW";
+    },
+
+    // Schedule update.
+    scheduleUpdate: function () {
+        var self = this;
+        setInterval(function () {
+            self.sendSocketNotification("GET_EVCC_DATA");
+            self.updateDom();
+        }, this.config.updateInterval * 1000);
+    },
+
+
 });
